c5ff591317f7836ceaf61ef0471e0e0c88d9a228
Rohan.s <roh142002@gmail.com>
2025-04-26 21:42:18 +0530

    01basicreact and 01vitereact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>Rohan's React Project </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+   <h1>Rohan's React Project with Vite </h1>
+  )
+}
+
+export default App
