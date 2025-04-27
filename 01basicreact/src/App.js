527e56573ce4f9609783122acb2bb54db8dba7b5
Rohan.s <roh142002@gmail.com>
2025-04-27 12:20:32 +0530

    Updated codes for react and vite and adding new files

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,11 @@
-
-
+import Chai from "./Chai";
 
 function App() {
   return (
+    <>
+    <Chai />
    <h1>Rohan's React Project </h1>
+   </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,21 @@
-
+import Chai from "./chai"
 
 
 
 
 function App() {
- 
+
 
   return (
-   <h1>Rohan's React Project with Vite </h1>
+    <>
+   <Chai /> 
+
+   <h1>Rohans React</h1>
+   <p>RVCE COLLEGE OF ENGINEERING</p>
+   <p>Test para</p>
+</>
+  
+   
   )
 }
 
